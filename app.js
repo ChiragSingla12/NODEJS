80cf700c5124f797580921cd4d089e3b490376ce
ChiragSingla12 <Chiragsingla1254@gmail.com>
2023-08-17 11:42:33 +0530

    url like home, about, node added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log('My name is chirag');
+    const url = req.url;
+
+    if(url === '/home'){
+        res.write('<h1>welcome home</h1>');
+    }
+    if(url === '/about'){
+        res.write('<h1>Welcome to About Us page</h1>');
+    }
+    if(url === '/node'){
+        res.write('<h1>Welcome to my Node Js project</h1>');
+    }
 });
 
 server.listen(4000);
