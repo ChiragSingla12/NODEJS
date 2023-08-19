e8dc8e8fbefcea8d6d976d5f8bd72ae76463d984
ChiragSingla12 <Chiragsingla1254@gmail.com>
2023-08-19 15:21:44 +0530

    Understanding the module system and  added routes.js

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+const routes = require('./routes');
 
-    if (url === '/') {
-
-        fs.readFile('message.txt', (err, data) => {
-            if (err) {
-                console.log(error);
-            }
-
-            res.write('<html>')
-            console.log(`data frm file ` + data);
-            res.write('<head><title>Enter Message</title></head>')
-            res.write(`<body>${data}</body>`)
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body> ')
-            res.write('</html>');
-            return res.end();
-        });
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunks) => {
-            console.log(chunks);
-            body.push(chunks);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-});
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
 
